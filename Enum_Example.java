083bbee2fa0641c570200fc25f99c4d97000ad24
Niket <niket1409@gmail.com>
2018-09-11 21:05:24 +0530

    Enum example added

--- Enum_Example.java
+++ Enum_Example.java
@@ -1,12 +1,14 @@
-/*package mypack;
+package mypack;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class Enum_Example {
 	
 	public static void main(String[] args)
 {
 	System.out.println(StateInfo.MAHARASTRA);
-	
+	System.out.println(StateInfo.matchfor("Mumbai"));
 }
 
 }
@@ -15,15 +17,39 @@ public class Enum_Example {
  enum StateInfo
 {
 	MAHARASTRA(500,"Mumbai"),
-	KARNATAKA(1000,"Benglaru");
+	KARNATAKA(1000,"Benglaru"),
+	 UNKNOWN(0,"UNKNOWN STATE");
 	
 	private final String capital;
 	private final int population; 
+	
+	private static  Map<String,String> reverselookup;
 	
 	private StateInfo(int population,String capital)
 	{
 		this.capital=capital;
 		this.population=population;
+		
+	}
+	
+	static
+	{
+		reverselookup = new HashMap<>();
+		for(StateInfo stateInfo : StateInfo.values())
+		{
+			reverselookup.put(stateInfo.capital, stateInfo.name());
+			
+		}
+	}
+	
+	public static String matchfor(String cap)
+	{
+		if(reverselookup.containsKey(cap))
+		{
+			return reverselookup.get(cap);
+		}
+		return UNKNOWN.name();
+		
 	}
 	
 	public int getpopulation()
@@ -35,13 +61,7 @@ public class Enum_Example {
 	{
 		return this.capital;
 	}
-	public static StateInfo[] getEnumName(int population,String capital)
-	{
-		
-			return  StateInfo.values();
-		
-	}
-	
+  
 	public String toString()
 	{
 	return "Capital Name "+capital+" Population:"+population;
@@ -50,4 +70,4 @@ public class Enum_Example {
 	
 	
 	
-}*/
+}
